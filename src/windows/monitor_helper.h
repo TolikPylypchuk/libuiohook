af941e861c6398f00e63e206ee9469fdd9f95280
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2024-05-20 22:10:26 +0300

    Fix enumerating displays when posting mouse events on Windows

--- src/windows/monitor_helper.h
+++ src/windows/monitor_helper.h
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <Windows.h>
 
 typedef struct {
@@ -6,5 +7,7 @@ typedef struct {
 } largest_negative_coordinates;
 
 extern void enumerate_displays();
+
+extern void set_always_enumerate_displays(bool always);
 
 extern largest_negative_coordinates get_largest_negative_coordinates();
